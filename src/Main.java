396aeee3d1f8fd23159951297794493894a20dea
d.vlasov1 <dvlasov150@gmail.com>
2020-02-26 09:20:20 -0700

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(printDavid());
+        System.out.println("asdf");
     }
 
     public static String printDavid() {
