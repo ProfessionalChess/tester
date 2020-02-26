e5c124bccd904c2b2d6d136ffdf768f6acd8062d
professionalChess <m.chizhov@share.epsb.ca>
2020-02-26 09:28:48 -0700

    Hey

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Mikhail");
+        System.out.println("David");
         System.out.println("David");
     }
 
