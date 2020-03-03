cc9b498aa334c72762b46a475529c1fc45941e27
professionalChess <m.chizhov@share.epsb.ca>
2020-03-03 09:47:32 -0700

    Hey

--- src/gui/chessMain.java
+++ src/gui/chessMain.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class chessMain {
     public static void main(String[] args) {
         JFrame frame = new menu();
-        //JFrame frame = new board();
-        ArrayList<JButton> asdfa = new ArrayList();
+
     }//end main
 }//end class
